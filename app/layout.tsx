05180ea897775778d2c35d6393b08cd81041cc88
paulpriyanshu <priyanshu.paul003@gmail.com>
2026-01-18 23:04:22 +0530

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Playfair_Display, Lora } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 
 const playfair = Playfair_Display({
@@ -49,6 +50,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${playfair.variable} ${lora.variable} font-serif antialiased`}>
         {children}
+        <SpeedInsights/>
         <Analytics />
       </body>
     </html>
