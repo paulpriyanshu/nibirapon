5ed304f2c1c6790dc8cc0371352bc44521bcf894
paulpriyanshu <priyanshu.paul003@gmail.com>
2026-01-19 00:20:42 +0530

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,21 +22,8 @@ export const metadata: Metadata = {
   keywords:
     "sarees, suit pieces, Indian ethnic wear, traditional clothing, wholesale, saree exporter, textile supplier",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/icon.png"
+
   },
     generator: 'v0.app'
 }
